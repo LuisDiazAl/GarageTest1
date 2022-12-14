7a22f36bf1856a09a4084424eb1e5d1280eb3a38
LuisDiazAl <484diazluis@gmail.com>
2022-12-14 17:11:20 -0300

    fix method generarPartidos

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import Mundial.*;
 import Vehiculo.*;
 import com.sun.xml.internal.ws.wsdl.writer.document.Part;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Main {
@@ -23,16 +24,30 @@ public class Main {
 
     System.out.println(String.format("Esta full¿?: %b \nCapacidad Actual: %s\nPromedio de Km: %f\nPrecio Total de Ruedas: %f", garage1.estaFull(), garage1.getListaDeVehiculo().size(),garage1.promedioKm(),garage1.precioTotalRuedas()));
 
+
         System.out.println("----MUNDIAL-----");
 
-        Equipo equipo1 = new Equipo("Palermo");
-        Equipo equipo2 = new Equipo("Recoleta");
+        ArrayList<Equipo> teamgrupoA = new ArrayList<Equipo>();
+        teamgrupoA.add(new Equipo("ARG"));
+        teamgrupoA.add(new Equipo("BELG"));
+        teamgrupoA.add(new Equipo("ESP"));
+        teamgrupoA.add(new Equipo("USA"));
 
-        Partido partido1 = new Partido(new Date(22,05,10),equipo1,equipo2);
+        ArrayList<Equipo> teamgrupaB = new ArrayList<Equipo>();
+        teamgrupaB.add(new Equipo("PSG"));
+        teamgrupaB.add(new Equipo("BAR"));
+        teamgrupaB.add(new Equipo("MAD"));
+        teamgrupaB.add(new Equipo("ATH"));
 
-        partido1.hacerGoles(1,2);
+        Grupo primerGrupo = new Grupo();
+        
 
-        System.out.println(String.format("Partido 1: %d",partido1.getResultado()));
+
+        Grupo segundoGrupo = new Grupo();
+
+
+
+
 
 
 
--- src/Mundial/Grupo.java
+++ src/Mundial/Grupo.java
@@ -1,10 +1,24 @@
 package Mundial;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Grupo extends EtapaMundial{
     @Override
     public ArrayList<Equipo> getEquiposQueAvanzan() {
         return super.getEquiposQueAvanzan();
+    }
+
+    public void generarPartidos(ArrayList<Equipo> equipos) {
+        for (int i = 0; i < equipos.size() + 1; i++) {
+
+            for (int j = i+1; j < equipos.size(); j++) {
+
+                getListaDepartidos().add(new Partido(new Date(), equipos.get(i), equipos.get(j)));
+            }
+
+        }
+
+
     }
 }
